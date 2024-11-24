e9f53ad19748f440e27e534a92d7e282ef122623
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-11-24 12:19:54 +0530

    UPDATE!

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from './ui/button'
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  SignIn,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/clerk-react";
+import { PenBox } from "lucide-react";
 
 const header = () => {
+   const [showSignIn, setshowSignin] = useState(false);
+
+   const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setshowSignin(false);
+    }
+   }
+
   return (
     <>
-    <nav className=' py-4 flex justify-between items-center'>
-       <Link>
-         <img src="./dixhire.png" className=' h-16' />
-       </Link>
+      <nav className=" py-4 flex justify-between items-center">
+        <Link>
+          <img src="./dixhire.png" className=" h-16" />
+        </Link>
 
-       <Button variant="outline">Button</Button>
-      
-      {/*
-    <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-       
-    */}
-       
 
-    </nav>
-    
+        <div className=" flex gap-8">
+          <SignedOut>
+            <Button variant="outline"   onClick={()=>setshowSignin(true)}>Login</Button>
+          </SignedOut>
+          
+          <SignedIn>
+          {/* add a condition here */}
+         
+          <Button
+          variant='destructive' 
+          className=' rounded-full'
+        
+          >
+          <PenBox size={20} className="mr-2" />
+          Post Job
+         </Button>
+          <Link to='/postjob'>
+          </Link>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+      {showSignIn && 
+        <div className=" fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+        onClick={handleOverlayClick}
+        >
+          <SignIn
+          signUpForceRedirectUrl="/onbording"
+          fallbackRedirectUrl="/onbording"
+          />
+        </div>}
     </>
-  )
-}
+  );
+};
 
-export default header
+export default header;
--- src/pagess/Startpage.jsx
+++ src/pagess/Startpage.jsx
@@ -1,67 +1,112 @@
-import React from 'react'
-import './../App.css'
-import { Link } from 'react-router-dom'
-import { Button } from '@/components/ui/button'
-import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel'
-import companies from '../data/companies.json'
-import Autoplay from 'embla-carousel-autoplay'
+import React from "react";
+import "./../App.css";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import companies from "../data/companies.json";
+import faqs from "../data/faq.json";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const Startpage = () => {
   return (
-   <main className=' flex flex-col gap-10 sm:gap-20 py-20 sm:py-20'>
-     <section className=' text-center'>
-       <h1 className=' flex flex-col  items-center justify-center gradient-title text-3xl font-extrabold sm:text-6xl lg:text-6xl'> 
-       Find Your Dream Job{""}
-       <span className=' flex items-center gap-3 sm:gap-6'>
-       And Get{""}
-        <span className=' text-indigo-400 underline'> Hired</span>
-        </span> 
+    <main className=" flex flex-col gap-10 sm:gap-20 py-20 sm:py-20">
+      <section className=" text-center">
+        <h1 className=" flex flex-col  items-center justify-center gradient-title text-3xl font-extrabold sm:text-6xl lg:text-6xl">
+          Find Your Dream Job{""}
+          <span className=" flex items-center gap-3 sm:gap-6">
+            And Get{""}
+            <span className=" text-indigo-400 underline"> Hired</span>
+          </span>
         </h1>
-        <p className=' text-gray-300 sm:mt-4 md:mt-4 lg:mt-6 text-xs sm:text-xl'>
-        Discover a multitude of job opportunities or identify the ideal candidate.
+        <p className=" text-gray-300 sm:mt-4 md:mt-4 lg:mt-6 text-xs sm:text-xl">
+          Discover a multitude of job opportunities or identify the ideal
+          candidate.
         </p>
-     </section>
+      </section>
 
-     <div className=' flex gap-6  justify-center'>
-         {/* buttons */}
+      <div className=" flex gap-6  justify-center">
+        {/* buttons */}
 
-         <Link to='/jobs'>
-            <Button variant="blue" size="xl">Find Jobs</Button>
-         </Link>
+        <Link to="/jobs">
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
+        </Link>
 
-         <Link to='/postjob'>
-            <Button variant="destructive" size="xl">Post Job</Button>
-         </Link>
-       
-    </div>
-          {/* carousel */}
-          <Carousel
-          plugins={[Autoplay({ delay: 2000 })]}
-          className="w-full py-10">
-            <CarouselContent className=" flex gap-5 sm:gap-20 items-center">
-              {companies.map(({ name, id, path }) => {
-                return (
-                  <CarouselItem key={id} className=" basis-1/3 lg:basis-1/6">
-                    <img 
-                    src={path} 
-                    alt={name} 
-                    className=' h-9 sm:h-14 object-contain' 
-                    />
-                  </CarouselItem>
-                )
-              })}
-            </CarouselContent>
-          </Carousel>
-        {/* banner */}
-        <img src="./banner.jpg" className='w-full' alt="" />
+        <Link to="/postjob">
+          <Button variant="destructive" size="xl">
+            Post Job
+          </Button>
+        </Link>
+      </div>
+      {/* carousel */}
+      <Carousel plugins={[Autoplay({ delay: 2000 })]} className="w-full py-10">
+        <CarouselContent className=" flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => {
+            return (
+              <CarouselItem key={id} className=" basis-1/3 lg:basis-1/6">
+                <img
+                  src={path}
+                  alt={name}
+                  className=" h-9 sm:h-14 object-contain"
+                />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
+      {/* banner */}
+      <img src="./banner.jpg" className="w-full" alt="" />
+
+      <section className=" grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>For Job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent className=" font-semibold">
+            Explore job openings and submit your applications, keep track of
+            your application progress, and participate in various other tasks
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>For Employers</CardTitle>
+          </CardHeader>
+          <CardContent className=" font-semibold">
+            Publish job listings, coordinate application management, and select
+            the top candidates.
+          </CardContent>
+        </Card>
+      </section>
+
+      {/* Accordian */}
+      <Accordion type="single" collapsible>
+          {faqs.map((faq, index)=>{
+            return (
+              <AccordionItem key={index} value={`item-${index+1}`}>
+            <AccordionTrigger>{faq.question}</AccordionTrigger>
+            <AccordionContent>
+            {faq.answer}
+            </AccordionContent>
+          </AccordionItem>
+            )
+          })}
         
-       <section>
-       
-       </section>
+      </Accordion>
+    </main>
+  );
+};
 
-       {/* Accordian */}
-   </main>
-  )
-}
-
-export default Startpage
+export default Startpage;
