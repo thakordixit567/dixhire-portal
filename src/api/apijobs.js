4081f953009a7f7999bd517a8baf80467b72d3d6
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-11-27 12:17:01 +0530

    UPDATE!

--- src/api/apijobs.js
+++ src/api/apijobs.js
@@ -1,3 +1,16 @@
-export async function getJobs(token) {
+import supabaseClient from "@/utils/supabase";
 
-} 
+export async function getJobs(token) {
+  const supabase = await supabaseClient(token);
+
+  let query = supabase.from("jobs").select("*");
+
+  const { data, error } = await query;
+
+  if (error) {
+    console.error("Error fetching jobs:", error);
+    return null;
+  }
+
+  return data;
+}
--- src/pagess/joblisting.jsx
+++ src/pagess/joblisting.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { getJobs } from '@/api/apijobs'
+
+import React, { useEffect } from 'react'
 
 const joblisting = () => {
+  
+
+
+  const fetchJobs = async()=>{
+    const supabaseAccessToken = await session.getToken({
+       template: "supabase",
+    });
+    
+   const data = await getJobs(supabaseAccessToken);
+   console.log(data);
+  };
+
+
+ useEffect(() => {
+    fetchJobs()
+ }, []);
+
   return (
     <div>
     joblisting
