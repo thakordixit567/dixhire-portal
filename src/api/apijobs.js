e8a60ab3c0626c8ff8f5be90d5a8699757a2c3b8
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-11-30 18:43:29 +0530

    UPDATE!

--- src/api/apijobs.js
+++ src/api/apijobs.js
@@ -3,7 +3,9 @@ import supabaseClient from "@/utils/supabase";
 export async function getJobs(token, { location, company_id, searchQuery }) {
   const supabase = await supabaseClient(token);
 
-  let query = supabase.from("jobs").select("*");
+  let query = supabase
+  .from("jobs")
+  .select("*, company:companies(name,logo_url),saved: saved_jobss(id)");
 
   if (location) {
     query = query.eq("location", location);
--- src/components/jobcard.jsx
+++ src/components/jobcard.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useUser } from "@clerk/clerk-react";
+import React from "react";
+import { Card, CardHeader, CardTitle } from "./ui/card";
+import { Trash2Icon } from "lucide-react";
 
-const jobcard = () => {
+const jobcard = ({
+  job,
+  isMyJob = false,
+  savedInt = false,
+  onJobSaved = () => {},
+}) => {
+  const { user } = useUser();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Card>
+      <CardHeader>
+         <CardTitle>{job.title}</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
 
-export default jobcard
+export default jobcard;
