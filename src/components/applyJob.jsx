fea5e8886880061810a1fa3648359c95a4230d41
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-12-02 17:41:47 +0530

    UPDATE!

--- src/components/applyJob.jsx
+++ src/components/applyJob.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Button } from "./ui/button";
 
-const ApplyJobDrawer = () => {
-  return <div>applyJob applyJob</div>;
+
+const ApplyJobDrawer = ({ user, jobs, fetchJob, applied = false}) => {
+  return (
+
+    <Drawer open={applied ? false : undefined}>
+  <DrawerTrigger>
+    <Button
+     variant={jobs?.isOpen && !applied ? "blue" : "destructive"}
+     disabled={!jobs?.isOpen || applied}
+     size="lg"
+     >
+      {jobs?.isOpen ? (applied ? "Applied" : "Apply") : "Hiring Closed"}
+    </Button>
+  </DrawerTrigger>
+  <DrawerContent>
+    <DrawerHeader>
+      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+      <DrawerDescription>This action cannot be undone.</DrawerDescription>
+    </DrawerHeader>
+    <DrawerFooter>
+      <Button>Submit</Button>
+      <DrawerClose>
+        <Button variant="outline">Cancel</Button>
+      </DrawerClose>
+    </DrawerFooter>
+  </DrawerContent>
+</Drawer>
+
+  );
 };
 
 export default ApplyJobDrawer;
