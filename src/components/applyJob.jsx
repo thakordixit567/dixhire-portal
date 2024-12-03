a903c0bb79183f871000c1c942560bcb7a181b62
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-12-03 21:32:04 +0530

    UPDATE!

--- src/components/applyJob.jsx
+++ src/components/applyJob.jsx
@@ -8,37 +8,51 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer"
+} from "@/components/ui/drawer";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
-
-const ApplyJobDrawer = ({ user, jobs, fetchJob, applied = false}) => {
+const ApplyJobDrawer = ({ user, jobs, fetchJob, applied = false }) => {
   return (
-
     <Drawer open={applied ? false : undefined}>
-  <DrawerTrigger>
-    <Button
-     variant={jobs?.isOpen && !applied ? "blue" : "destructive"}
-     disabled={!jobs?.isOpen || applied}
-     size="lg"
-     >
-      {jobs?.isOpen ? (applied ? "Applied" : "Apply") : "Hiring Closed"}
-    </Button>
-  </DrawerTrigger>
-  <DrawerContent>
-    <DrawerHeader>
-      <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-      <DrawerDescription>This action cannot be undone.</DrawerDescription>
-    </DrawerHeader>
-    <DrawerFooter>
-      <Button>Submit</Button>
-      <DrawerClose>
-        <Button variant="outline">Cancel</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer>
+      <DrawerTrigger asChild>
+        <Button
+          variant={jobs?.isOpen && !applied ? "blue" : "destructive"}
+          disabled={!jobs?.isOpen || applied}
+          size="lg"
+        >
+          {jobs?.isOpen ? (applied ? "Applied" : "Apply") : "Hiring Closed"}
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>
+            Apply For {jobs?.title} at {jobs?.company?.name}
+          </DrawerTitle>
+          <DrawerDescription>Please Fill Up The below form.</DrawerDescription>
+        </DrawerHeader>
 
+        <form className=" flex flex-col gap-4 p-4 pb-0">
+          <Input
+            type="number"
+            placeholder="Years Of Experiance"
+            className=" flex-1"
+          />
+          <Input
+            type="text"
+            placeholder="Skills (Comma Seprated)"
+            className=" flex-1"
+          />
+        </form>
+
+        <DrawerFooter>
+          <Button>Submit</Button>
+          <DrawerClose>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
